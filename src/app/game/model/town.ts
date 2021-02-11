9da4c9382f0f7c6805d652f125c46ad32e0caec8
Venusta <Venusta>
2021-02-11 11:48:53 +0000

    dfgdafg

--- src/app/game/model/town.ts
+++ src/app/game/model/town.ts
@@ -1,9 +1,16 @@
-import { TownInterface, Resources, ResearchList, UnitList, BuildingList } from "../../../types/types";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { TownInterface, Resources, ResearchList, UnitList, BuildingList, Building } from "../../../types/types";
 import { BuildingId } from "../constants";
 import { baseBuildings } from "../buildings";
 import { ResourceBuilding } from "./resourceBuilding";
 
 const defaultResources = { timber: 0, clay: 0, iron: 0 };
+
+const defaultBuilding = (id: BuildingId, level = 0, queuedLevel = 0) => ({
+  id,
+  level,
+  queuedLevel,
+})
 
 export class Town {
   name: string;
@@ -17,18 +24,31 @@ export class Town {
   units: UnitList;
   buildings: BuildingList;
 
-  constructor({ id, name, resources = defaultResources, unlocked, units, buildings }: TownInterface) {
+  constructor({ id, name, resources = defaultResources, unlocked, units }: TownInterface) {
     this.id = id
     this.name = name // todo random name
     this.resources = resources;
-    this.buildings = buildings;
     this.unlocked = unlocked;
     this.units = units;
+    this.buildings = this.defaultBuildings();
     this.rps = this.calculateResourcesPerSecond();
     this.population = this.getPopulation();
     this.maxPopulation = this.calculateMaxPopulation();
     this.storageCapacity = this.calculateStorageCapacity();
   };
+
+  private defaultBuildings(): BuildingList {
+    return {
+      [BuildingId.TimberCamp]: defaultBuilding(BuildingId.TimberCamp),
+      [BuildingId.ClayPit]: defaultBuilding(BuildingId.ClayPit),
+      [BuildingId.IronMine]: defaultBuilding(BuildingId.IronMine),
+      [BuildingId.Barracks]: defaultBuilding(BuildingId.Barracks),
+      [BuildingId.Stable]: defaultBuilding(BuildingId.Stable),
+      [BuildingId.Farm]: defaultBuilding(BuildingId.Farm),
+      [BuildingId.Warehouse]: defaultBuilding(BuildingId.Warehouse),
+      [BuildingId.Headquarters]: defaultBuilding(BuildingId.Headquarters),
+    }
+  }
 
   private getPopulation(): number {
     let totalWorkers = 0;
@@ -50,22 +70,20 @@ export class Town {
   }
 
   private calculateResourcesPerSecond(): Resources {
+    const rps: Resources = { timber: 0, clay: 0, iron: 0 }
+    Object.values(this.buildings).forEach(({ id, level }) => {
+      const buildingData = baseBuildings[id];
 
-    // Object.values(this.buildings).forEach(({ id, level }) => { // TODO FIX
-    //   const buildingData = baseBuildings[id];
-
-    //   if (buildingData instanceof ResourceBuilding) {
-    //     const newResourcesPerSecond = buildingData.getResourceGeneration(level);
-    //     buildingData.creates.forEach((resource) => {
-    //       this.rps[resource] += newResourcesPerSecond;
-    //       console.log(`Adding: ${newResourcesPerSecond}`);
-    //       console.log(`${resource} is now ${this.rps[resource]} per second`);
-    //     });
-    //   };
-
-    // })
-
-    return { timber: 0, clay: 0, iron: 0 };
+      if (buildingData instanceof ResourceBuilding) {
+        const newResourcesPerSecond = buildingData.getResourceGeneration(level);
+        buildingData.creates.forEach((resource) => {
+          rps[resource] += newResourcesPerSecond;
+          console.log(`Adding: ${newResourcesPerSecond}`);
+          console.log(`${resource} for ${this.id} is now ${rps[resource]} per second`);
+        });
+      };
+    })
+    return rps;
   }
 
   public toRedux(): TownInterface {
