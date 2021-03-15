062fd08f5199e50df06a4f7e578d669f03f5920d
Venusta <Venusta>
2021-03-15 18:23:46 +0000

    .

--- src/app/components/Overview/index.tsx
+++ src/app/components/Overview/index.tsx
@@ -3,22 +3,22 @@ import { selectTowns } from "../../selectors";
 import { useStateSelector } from "../hooks";
 import Style from "./style.module.css";
 
+const Resources = ({ resources }: { resources: ResourcesNormalised }): JSX.Element => (
+  <div>
+    {Object.values(resources.id).map((resource) => {
+      if (resource !== undefined) {
+        return (
+          <div>{`${resource.id}: ${resource.amount}`}</div>
+        );
+      }
+      return <></>;
+    })}
+  </div>
+);
+
 export const Overview = (): JSX.Element => {
   const towns = useStateSelector((state) => selectTowns(state));
   // Object.values(towns.byId)
-
-  const Resources = ({ resources }: { resources: ResourcesNormalised }): JSX.Element => (
-    <div>
-      {Object.values(resources.id).map((resource) => {
-        if (resource !== undefined) {
-          return (
-            <div>{`${resource.id}: ${resource.amount}`}</div>
-          );
-        }
-        return <></>;
-      })}
-    </div>
-  );
 
   return (
     <div className={Style.container}>
